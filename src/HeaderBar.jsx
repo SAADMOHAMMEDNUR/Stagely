a66397688975441f961030c0464196bf51617e7c
Samarth Kunhody <skunhody@uw.edu>
2026-03-09 16:09:02 -0700

    rendered navbar in inline expression

--- src/HeaderBar.jsx
+++ src/HeaderBar.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import { Link } from "react-router";
 
 export function Header() {
+  const NavLinks = [
+    { link: "index.html", text: "Home" },
+    { link: "voting-artists.html", text: "Artists" },
+    { link: "voting-commercials.html", text: "Commercials" },
+    { link: "voting-bingo.html", text: "Make Your Own Bingo" },
+  ]
+  const navItems = NavLinks.map((item) => (
+    <a href={item.link}>
+      {item.text}
+    </a>
+  ));
   return (
     <header>
       <div className="container">
         <nav className="nav-bar">
-          <Link to="/artists">Artists</Link>
-          <Link to="/commercials">Commercials</Link>
-          <Link to="/bingo-settings">Make Your Own Bingo</Link>
-        </nav>
+  {navItems}
+</nav>
 
         <div className="container text-center">
           <Link to="/" className="site-title">
